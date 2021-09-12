617e5fa6e9f882ad2bf6f2f9bbcf11b099729ae4
hamaccs <haldmadcoder@gmail.com>
2021-09-12 10:03:43 +0530

    "orientation managed"

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:bmi_calculator/views/input_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(const BMICalculator());
 
-class BMICalculator extends StatelessWidget
-{
+class BMICalculator extends StatelessWidget {
   const BMICalculator({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown,],);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
